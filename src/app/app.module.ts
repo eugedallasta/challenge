f7a357ba71e5c6b89bef6440a0e6aa2b1a49fd11
Eugenia Dallasta <mariaedallasta@gmail.com>
2022-08-24 12:28:23 -0300

    create project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
+import { FormAddUserComponent } from './components/form-add-user/form-add-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    UsersListComponent,
+    FormAddUserComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/form-add-user/form-add-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-add-user',
+  templateUrl: './form-add-user.component.html',
+  styleUrls: ['./form-add-user.component.scss']
+})
+export class FormAddUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
